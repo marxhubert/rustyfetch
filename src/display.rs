f558a60b513e90a5210b6c4e8fdfe625df017aa5
HERINDRAINY Marx <marx.herindrainy@businessdecision.com>
2025-03-22 13:41:53 +0400

    Basics

---
+++ src/display.rs
@@ -0,0 +1,36 @@
+pub fn get_logo() -> String {
+    r#"
+       .-""""""""-.
+     .'          '.
+    /   RustFetch  \
+   : ,          , :'
+    `._         _.'
+       `"'"""""'"` 
+    "#.to_string()
+}
+
+pub fn format_output(logo: String, infos: Vec<String>) -> String {
+    let logo_lines: Vec<&str> = logo.lines().collect();
+    let logo_width = logo_lines.iter().map(|line| line.len()).max().unwrap_or(0);
+    let max_lines = logo_lines.len().max(infos.len());
+
+    let diff = max_lines - logo_lines.len().min(infos.len());
+    let top = (diff - (diff % 2)) / 2;
+    let bottom = diff - top;
+
+    let mut bloc_to_ajust = vec![""; max_lines];
+
+    for (i, info) in infos.iter().enumerate() {
+        bloc_to_ajust[top + i] = info;
+    }
+
+    let mut output = String::new();
+
+    for i in 0..max_lines {
+        let logo_line = logo_lines.get(i).unwrap_or(&"");
+        let info_line = bloc_to_ajust.get(i).unwrap_or(&"");
+        output.push_str(&format!("{:<width$} {}\n", logo_line, info_line, width = logo_width + 3));
+    }
+    
+    output
+}
