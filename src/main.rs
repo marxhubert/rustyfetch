f4fbbd03241d74ff2d9bd1d54232d1f208f430a4
HERINDRAINY Marx <marx.herindrainy@businessdecision.com>
2025-03-23 17:01:59 +0400

    Add uptime

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ fn main() {
     let hostname = system::get_hostname();
     let cpu = system::get_cpu();
     let memory = system::get_memory();
+    let uptime = system::get_uptime();
 
     let infos = vec![
         format!("OS: {}", os),
         format!("Host: {}", hostname),
         format!("CPU: {}", cpu),
         format!("Memory: {}", memory),
+        format!("Uptime: {}", uptime),
     ];
     
     let logo = logo::get_logo();
