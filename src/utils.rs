77b5246314226f49ca5ef7fd0da3768334a03f13
HERINDRAINY Marx <marx.herindrainy@businessdecision.com>
2025-03-28 02:05:39 +0400

    Refacto

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use crate::theme;
 
 pub fn format_bytes(input: u64) -> String {
     let units = ["", "K", "M", "G", "T", "P", "E"];
@@ -10,4 +11,20 @@ pub fn format_bytes(input: u64) -> String {
     } else {
         format!("{:.2} {}B", value, units[factor])
     }
+}
+
+pub fn format_bar(used: u64, total: u64) -> String {
+    let percentage = if total > 0 { (used as f64 / total as f64) * 100.0 } else { 0.0 };
+    let bar_length = 10;
+    let filled = (percentage / 100.0 * bar_length as f64).round() as usize;
+    let empty = bar_length - filled;
+
+    format!(
+        "[{}{}] {} / {} ({})",
+        theme::primary(&"#".repeat(filled)),
+        ".".repeat(empty),
+        format_bytes(used),
+        format_bytes(total),
+        theme::primary(&format!("{:.0}%", percentage))
+    )
 }
